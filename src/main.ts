041746d64f869e82b6465d29190ff574f7198b5f
emfo2405 <emfo2405@student.miun.se>
2025-04-15 17:51:22 +0200

    HTML-formulär skapat

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,20 @@ interface Todo {
     completed: boolean,
     priority: 1 | 2 | 3;
 }
+
+//Skapar en klass som heter ToDoList
+class ToDoList {
+    //Skapar en array där inmatade objekt kan lagras
+    private todos: Todo[] = [];
+
+    //Skapar en konstruktor 
+    constructor(todos: Todo[]) {
+        this.todos = [];
+
+    }
+
+    //addTodo(task: string, priority: number): boolean {
+    //    if(task != "" && priority === 1 || 2 || 3 )
+    //        return true
+    //} 
+}
